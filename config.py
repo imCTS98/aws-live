49fd143b16f5707a90ec73b2921cf53e4f164678
imCTS98 <77603337+imCTS98@users.noreply.github.com>
2022-08-30 14:19:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c9axgcjhwh1z.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheongtecksing-employee"
+customregion = "us-east-1"
